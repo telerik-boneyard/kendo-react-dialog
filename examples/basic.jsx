f10e874ee67b8394b7837165e71546220edfe554
Alex Gyoshev <alex@gyoshev.net>
2016-02-15 18:13:49 +0200

    feat: reuse button component

--- examples/basic.jsx
+++ examples/basic.jsx
@@ -1,14 +1,11 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import KendoDialog from '../src/kendo-dialog';
+import KendoDialog from '../src/KendoDialog';
 
 ReactDOM.render(
   <KendoDialog title="Loss of data">
     <p>This operation will result in loss of data.</p>
     <p>Are you sure you want to continue?</p>
-
-    <button className="k-button k-primary">Yep</button>
-    <button className="k-button">Nope</button>
   </KendoDialog>,
   document.getElementById('app')
 );
--- src/KendoDialog.jsx
+++ src/KendoDialog.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
-import styles from '@telerik/kendo-theme-default/styles/main';
+import styles from '@telerik/kendo-theme-default/styles/dialog/main';
+import KendoButton from '@telerik/kendo-react-button/src/bundle';
 import KendoDialogTitleBar from '../src/KendoDialogTitleBar';
 
 const KendoDialog = (props) => (
@@ -9,6 +10,11 @@ const KendoDialog = (props) => (
 
         <div className={styles.content + " " + styles['window-content']}>
             {props.children}
+
+            <div className={styles['action-buttons']}>
+                <KendoButton primary>Yep</KendoButton>
+                <KendoButton>Nope</KendoButton>
+            </div>
         </div>
     </div>
 );
