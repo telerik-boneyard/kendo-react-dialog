af43952b544bee294cf9b92f6fb87a3da101dda0
Alex Gyoshev <alex@gyoshev.net>
2016-03-29 18:24:05 +0300

    feat: use animation package and animation styles

--- src/Dialog.jsx
+++ src/Dialog.jsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 
 import styles from '@telerik/kendo-theme-default/styles/dialog/main';
-//import Animation from '@telerik/kendo-react-animation';
+import { Animation } from '@telerik/kendo-react-animation';
 import DialogTitleBar from '../src/DialogTitleBar';
 import DialogActions from '../src/DialogActions';
 import classNames from 'classnames';
 
 const Dialog = (props) => {
-    const wrapperClasses = classNames(styles.widget, styles.window);
+    const wrapperClasses = classNames(styles.widget, styles.window, styles.centered);
 
     const contentClasses = classNames(styles.content, styles['window-content']);
 
@@ -26,10 +26,14 @@ const Dialog = (props) => {
     }
 
     return (
-        <div>
+        <div className={styles['dialog-wrapper']}>
             {overlay}
 
-            {/*<Animation effect="k-top">*/}
+            <Animation
+                transitionAppear
+                transitionAppearTimeout={300}
+                transitionName={styles['slide-in-appear']}
+            >
                 <div className={wrapperClasses}>
                     <DialogTitleBar onClose={props.onClose}>
                         {props.title}
@@ -41,7 +45,7 @@ const Dialog = (props) => {
 
                     {actions}
                 </div>
-            {/*</Animation>*/}
+            </Animation>
         </div>
     );
 };
