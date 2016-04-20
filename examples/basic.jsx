f6296d6947ac92e94333763896d67dc3aa2d5a9d
Alex Gyoshev <alex@gyoshev.net>
2016-04-20 11:49:25 +0300

    docs: update basic example, remove modal

--- examples/basic.jsx
+++ examples/basic.jsx
@@ -1,11 +1,51 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { Dialog } from '@telerik/kendo-react-dialog';
+import { Button } from '@telerik/kendo-react-buttons';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { sessionExpired: true };
+        this.openHandler = this.onOpen.bind(this);
+        this.closeHandler = this.onClose.bind(this);
+    }
+
+    onClose() {
+        this.setState({ sessionExpired: false });
+    }
+
+    onOpen() {
+        this.setState({ sessionExpired: true });
+    }
+
+    render() {
+        const dialog = (
+            <Dialog
+                actions={[ 'OK' ]}
+                modal={true}
+                onClose={this.closeHandler}
+                title="Session expired"
+            >
+              <p>Your session has expired.</p>
+              <p>You have been signed out.</p>
+            </Dialog>
+        );
+
+        const button = (
+            <Button onClick={this.openHandler}>Open dialog</Button>
+        );
+
+        return (
+            <section>
+                { this.state.sessionExpired ? dialog : button }
+            </section>
+        );
+    }
+};
+
 
 ReactDOM.render(
-  <Dialog actions={[ 'OK' ]} title="Loss of data">
-    <p>This operation will result in loss of data.</p>
-    <p>Are you sure you want to continue?</p>
-  </Dialog>,
+  <App />,
   document.getElementById('app')
 );
